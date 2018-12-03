bb26c433de3319e1fc3fd461f8a928112b4e57ae
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2018-12-03 13:43:38 +0100

    commit before presentation

--- lab_2/test_mathieu/Data.py
+++ lab_2/test_mathieu/Data.py
@@ -2,12 +2,16 @@
 
 import Importer
 
-class Data():
+class Data(Dict):
     """The representation in memory of the data of a sensor"""
 
-    def __init__(self):
-        pass
-
-    def get_data(self, importer, data_file):
-        self.data = importer.from_file(data_file)
+    def __init__(self, data = {}):
+        """Currently the implementation is the one of a dictionary of tuples"""
+        Dict.__init__(self, data)
+        
+    def add_data(self, time, value):
+        if time in self and self[time] != value:
+            raise ValueError("Can't have two values for the same time")
+        self[time] = value
+    
 
