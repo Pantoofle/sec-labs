4a75ccacc5e88c15dc6984a693c87ad3da1a07e3
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2018-12-05 16:18:34 +0100

    utils: first data object

---
+++ lab_2/sensim/utils/data.py
@@ -0,0 +1,12 @@
+class Data:
+    """Defines a point of data. Has all the information needed to plot and use
+    this data in the Simulation, or plot it in a Display"""
+
+    def __init__(self, timestamp, **kwargs):
+        self.timestamp = timestamp
+        self.data = dict()
+        for key, value in kwargs.iteritems():
+            self.data[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
