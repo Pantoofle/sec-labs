49bf11dfa0a2f99287838de9f046a9a672fe2484
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2018-12-05 16:34:12 +0100

    data: add timestamp modifications

--- lab_2/sensim/utils/data.py
+++ lab_2/sensim/utils/data.py
@@ -1,12 +1,18 @@
-class Data:
+class Data():
     """Defines a point of data. Has all the information needed to plot and use
     this data in the Simulation, or plot it in a Display"""
 
     def __init__(self, timestamp, **kwargs):
+        self.timestamp = timestamp
         self.data = dict()
-        self.data[timestamp] = timestamp
         for key, value in kwargs.iteritems():
             self.data[key] = value
 
     def __getitem__(self, key):
         return self.data[key]
+
+    def scaleTime(self, factor):
+        self.timestamp *= factor
+
+    def shiftTime(self, delta):
+        self.timestamp += delta
