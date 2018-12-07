54703c6fa6d261003fa0691198a38a8a11eff6f9
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2018-12-07 14:01:35 +0100

    ajout de commentaires

--- lab_2/sensim/main_test.py
+++ lab_2/sensim/main_test.py
@@ -14,7 +14,7 @@ m.transition = [[0.7, 0.3, 0], [0.1, 0.6, 0.3], [0.1, 0.3, 0]]
 rich = MarkovChain(name = "richSensor", speed=4, end=40)
 rich.addNodes(0,1,2,3)
 rich.setStartNode(0)
-rich.transition = [[0.2, 0.8, 0, 0],[0.1, 0.3, 0.6, 0], [0.1, 0.2, 0.3, 0.4],[0,0,0,1]]
+rich.transition = [[0.2, 0.8, 0, 0],[0.1, 0.3, 0.6, 0], [0.1, 0.2, 0.3, 0.4],[1,0,0,0]]
 
 aggr = AggregatedSensor(sensors = [m, rich], speed = 0.5)
 
