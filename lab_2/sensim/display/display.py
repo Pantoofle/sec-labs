1bc69e0022f8bdd35f8dceff27b3aebc2ac57681
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2018-12-11 22:50:46 +0100

    one typo

--- lab_2/sensim/display/display.py
+++ lab_2/sensim/display/display.py
@@ -4,8 +4,8 @@ from utils.data import Data
 class Display:
     """ Encapsulate all methods needed to communicate with external displays  """
     def __init__():
-        # For each sensor, we have a list of
-        self.sesors = dict()
+        # For each sensor, we have a list of 
+        self.sensors = dict()
 
     def addPlot(data, group):
         # If we land here, it is because the daughter class did not implement
