29ac41b0bc1b2d4fbcfd70ec0a82049d177a376c
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2018-12-21 18:45:19 +0100

    modification to make the makefile work

---
+++ lab_2/sensim/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name='sensim',
+        version='1.0',
+        author="Simon Fernandez, Mathieu Varille",
+        description="A graph toolbox implementing classical algorithms",
+        long_description="See a long description in the report pdf",
+        packages=setuptools.find_packages()
+)
