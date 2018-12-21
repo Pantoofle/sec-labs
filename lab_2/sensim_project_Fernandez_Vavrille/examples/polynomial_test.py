f53c2727807dafcb1b157687e586273d54690e8e
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2018-12-21 23:22:57 +0100

    Simon t'a fait n'importe quoi

--- lab_2/sensim_project_Fernandez_Vavrille/examples/polynomial_test.py
+++ lab_2/sensim_project_Fernandez_Vavrille/examples/polynomial_test.py
@@ -2,12 +2,10 @@ from sensim import *
 
 # Define Chain
 s = PolynomialSensor(name="poly_coef", coefs = [2, 0, 0])
-s1 = PolynomialSensor(name="poly_fun", polynomial = np.poly1d([2, 0, 0]))
-s2 = PolynomialSensor(name="poly_empty")
-s2.setPolynomial(np.poly1d([2, 0, 0]))
+s1 = PolynomialSensor(name="poly_fun", polynomial = np.poly1d([2, 1, 0]))
 
 display = JsonDisplay("polynomial_output.json")
 
-simu = Simulation(display = display, sensors = [s, s1, s2])
+simu = Simulation(display = display, sensors = [s, s1])
 
 simu.run()
